23b7633271ef1946f9f39c2c399bad937b3fc15f
Lakshmi Praba Stutzen <lakshmipraba@stutzen.me>
2020-06-26 06:36:42 +0000

    docker-php-entrypoint.sh edited online with Bitbucket

--- docker-php-entrypoint.sh
+++ docker-php-entrypoint.sh
@@ -6,20 +6,20 @@ if [ "${1#-}" != "$1" ]; then
         set -- apache2-foreground "$@"
 fi
 
-db_host=${DB_HOST}
-db_name=${DB_DATABASE}
-db_username=${DB_USERNAME}
-db_password=${DB_PASSWORD}
+db_host1=${DB_HOST1}
+db_name1=${DB_DATABASE1}
+db_username1=${DB_USERNAME1}
+db_password1=${DB_PASSWORD1}
 cp /var/www/opencart/config_root_prod.php /var/www/opencart/config.php
 cp /var/www/opencart/config_admin_prod.php /var/www/opencart/admin/config.php
 cp /var/www/opencart/mysqli_prod.php /var/www/opencart/system/library/db/mysqli.php
-sed -ri -e "s!env_db_host!$db_host!g" /var/www/opencart/config.php
-sed -ri -e "s!env_db_database!$db_name!g" /var/www/opencart/config.php
-sed -ri -e "s!env_db_username!$db_username!g" /var/www/opencart/config.php
-sed -ri -e "s!env_db_password!$db_password!g" /var/www/opencart/config.php
+sed -ri -e "s!env_db_host!$db_hos1t!g" /var/www/opencart/config.php
+sed -ri -e "s!env_db_database!$db_name1!g" /var/www/opencart/config.php
+sed -ri -e "s!env_db_username!$db_username1!g" /var/www/opencart/config.php
+sed -ri -e "s!env_db_password!$db_password1!g" /var/www/opencart/config.php
 
-sed -ri -e "s!env_db_host!$db_host!g" /var/www/opencart/admin/config.php
-sed -ri -e "s!env_db_database!$db_name!g" /var/www/opencart/admin/config.php
-sed -ri -e "s!env_db_username!$db_username!g" /var/www/opencart/admin/config.php
-sed -ri -e "s!env_db_password!$db_password!g" /var/www/opencart/admin/config.php
+sed -ri -e "s!env_db_host!$db_host1!g" /var/www/opencart/admin/config.php
+sed -ri -e "s!env_db_database!$db_name1!g" /var/www/opencart/admin/config.php
+sed -ri -e "s!env_db_username!$db_username1!g" /var/www/opencart/admin/config.php
+sed -ri -e "s!env_db_password!$db_password1!g" /var/www/opencart/admin/config.php
 exec "$@";
