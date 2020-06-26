decce37cfb6f5183a711f88b5ecb8b034bca2fd4
Lakshmi Praba Stutzen <lakshmipraba@stutzen.me>
2020-06-26 05:51:36 +0000

    docker-php-entrypoint.sh edited online with Bitbucket

--- docker-php-entrypoint.sh
+++ docker-php-entrypoint.sh
@@ -10,7 +10,7 @@ db_host=${DB_HOST}
 db_name=${DB_DATABASE}
 db_username=${DB_USERNAME}
 db_password=${DB_PASSWORD}
-cp /var/www/opencart/config_route_prod.php /var/www/opencart/config.php
+cp /var/www/opencart/config_root_prod.php /var/www/opencart/config.php
 cp /var/www/opencart/config_admin_prod.php /var/www/opencart/admin/config.php
 cp /var/www/opencart/mysqli_prod.php /var/www/opencart/system/library/db/mysqli.php
 sed -ri -e "s!env_db_host!$db_host!g" /var/www/opencart/config.php
